bb46572f773755aee01661f94a8b297b32c7d49b
Firdosh Homavazir <firdosh.homavazir@vector.co.nz>
2025-07-11 16:27:52 +1200

    test-ec-2-5

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "cidr" {
-  type    = list(any)
-  default = ["10.0.0.0/16"]
-}
+# variable "cidr" {
+#   type    = list(any)
+#   default = ["10.0.0.0/16"]
+# }
 
 variable "availability_zone" {
   type        = string
